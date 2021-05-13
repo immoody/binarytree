ddc8627bb4ee430f50c51b8d5d000b20b7a8c704
mertyilmaz <mertyilmaz@192.168.1.106>
2021-05-13 03:23:15 +0300

    Demo1

--- Project/src/Gui.java
+++ Project/src/Gui.java
@@ -6,11 +6,27 @@ import javax.swing.JTextArea;
 import javax.swing.JTextPane;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.GridLayout;
+import javax.swing.JSplitPane;
+import java.awt.Color;
+import javax.swing.SpringLayout;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Gui {
 
 	private JFrame frame;
+	private JTextField InsertTextfield;
+	private JButton InsertButton;
+	private JTextField DeleteTextfield;
+	private JButton DeleteButtonn;
+	private JButton DisplayButton;
 	private JTextArea textPane;
+	private JTextArea BoardTextPane;
 
 	/**
 	 * Launch the application.
@@ -40,24 +56,95 @@ public class Gui {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		 BinarySearchTree Binarytree = new BinarySearchTree();
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.getContentPane().setBackground(Color.BLACK);
+		frame.setBounds(100, 100, 600, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new CardLayout(0, 0));
+		SpringLayout springLayout = new SpringLayout();
+		frame.getContentPane().setLayout(springLayout);
 		
-		textPane = new JTextArea();
+		JLabel InsertLabel = new JLabel("Insert");
+		InsertLabel.setForeground(Color.CYAN);
+		springLayout.putConstraint(SpringLayout.WEST, InsertLabel, 10, SpringLayout.WEST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, InsertLabel, 66, SpringLayout.NORTH, frame.getContentPane());
+		frame.getContentPane().add(InsertLabel);
 		
-	
-		frame.getContentPane().add(textPane, "name_247169914846735");
-		BinarySearchTree x = new BinarySearchTree();
-		x.insert(5);
-		x.insert(3);
-		x.insert(12);
-		x.insert(7);
-		x.insert(6);
-		x.insert(8);
-		x.insert(8);
-		x.print2D(textPane);
+		JLabel lblDelete = new JLabel("Delete");
+		lblDelete.setForeground(Color.CYAN);
+		springLayout.putConstraint(SpringLayout.WEST, lblDelete, 0, SpringLayout.WEST, InsertLabel);
+		frame.getContentPane().add(lblDelete);
+		
+		InsertTextfield = new JTextField();
+		InsertTextfield.setBackground(Color.CYAN);
+		springLayout.putConstraint(SpringLayout.WEST, InsertTextfield, 111, SpringLayout.WEST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, InsertTextfield, -389, SpringLayout.EAST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.NORTH, InsertLabel, 0, SpringLayout.NORTH, InsertTextfield);
+		springLayout.putConstraint(SpringLayout.EAST, InsertLabel, -6, SpringLayout.WEST, InsertTextfield);
+		springLayout.putConstraint(SpringLayout.NORTH, InsertTextfield, 37, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, InsertTextfield, 66, SpringLayout.NORTH, frame.getContentPane());
+		frame.getContentPane().add(InsertTextfield);
+		InsertTextfield.setColumns(10);
+		
+		InsertButton = new JButton("Insert");
+		InsertButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Binarytree.insert(Integer.parseInt(InsertTextfield.getText()));
+				InsertTextfield.setText("");
+				
+			}
+		});
+		springLayout.putConstraint(SpringLayout.NORTH, InsertButton, 38, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, InsertButton, 11, SpringLayout.EAST, InsertTextfield);
+		springLayout.putConstraint(SpringLayout.SOUTH, InsertButton, -311, SpringLayout.SOUTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, InsertButton, -293, SpringLayout.EAST, frame.getContentPane());
+		frame.getContentPane().add(InsertButton);
+		
+		DeleteTextfield = new JTextField();
+		DeleteTextfield.setBackground(Color.CYAN);
+		springLayout.putConstraint(SpringLayout.NORTH, DeleteTextfield, 46, SpringLayout.SOUTH, InsertTextfield);
+		springLayout.putConstraint(SpringLayout.SOUTH, DeleteTextfield, -237, SpringLayout.SOUTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.NORTH, lblDelete, 6, SpringLayout.NORTH, DeleteTextfield);
+		springLayout.putConstraint(SpringLayout.EAST, lblDelete, -6, SpringLayout.WEST, DeleteTextfield);
+		springLayout.putConstraint(SpringLayout.WEST, DeleteTextfield, 111, SpringLayout.WEST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, DeleteTextfield, 0, SpringLayout.EAST, InsertTextfield);
+		DeleteTextfield.setColumns(10);
+		frame.getContentPane().add(DeleteTextfield);
+		
+		DeleteButtonn = new JButton("Delete");
+		DeleteButtonn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Binarytree.deleteKey(Integer.parseInt(DeleteTextfield.getText()));
+				DeleteTextfield.setText("");
+			}
+		});
+		springLayout.putConstraint(SpringLayout.NORTH, DeleteButtonn, 46, SpringLayout.SOUTH, InsertButton);
+		springLayout.putConstraint(SpringLayout.WEST, DeleteButtonn, 11, SpringLayout.EAST, DeleteTextfield);
+		springLayout.putConstraint(SpringLayout.EAST, DeleteButtonn, 96, SpringLayout.EAST, DeleteTextfield);
+		frame.getContentPane().add(DeleteButtonn);
+		
+		DisplayButton = new JButton("Display");
+		DisplayButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				BoardTextPane.setText("");
+				Binarytree.print2D(BoardTextPane);
+			}
+		});
+		springLayout.putConstraint(SpringLayout.NORTH, DisplayButton, 37, SpringLayout.SOUTH, DeleteTextfield);
+		springLayout.putConstraint(SpringLayout.WEST, DisplayButton, 10, SpringLayout.WEST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, DisplayButton, 120, SpringLayout.SOUTH, DeleteButtonn);
+		springLayout.putConstraint(SpringLayout.EAST, DisplayButton, 0, SpringLayout.EAST, InsertTextfield);
+		frame.getContentPane().add(DisplayButton);
+		
+		 BoardTextPane = new JTextArea();
+		BoardTextPane.setBackground(Color.CYAN);
+		springLayout.putConstraint(SpringLayout.NORTH, BoardTextPane, 0, SpringLayout.NORTH, InsertLabel);
+		springLayout.putConstraint(SpringLayout.WEST, BoardTextPane, 24, SpringLayout.EAST, InsertButton);
+		springLayout.putConstraint(SpringLayout.SOUTH, BoardTextPane, 0, SpringLayout.SOUTH, DisplayButton);
+		springLayout.putConstraint(SpringLayout.EAST, BoardTextPane, -10, SpringLayout.EAST, frame.getContentPane());
+		frame.getContentPane().add(BoardTextPane);
+		
+		
+		
 	}
-
 }
